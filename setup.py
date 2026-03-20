545e4a84e23835b3f1545df089fb9393f803f7c2
Dinesh Kumar <dinesh008luck@gmail.com>
2026-03-20 17:28:01 +0530

    inital commits

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import find_packages, setup
+
+setup(
+    name='src',
+    packages=find_packages(),
+    version='0.1.0',
+    description='Short Emotaion_Detection_v3',
+    author='Dinesh Kumar',
+    license='',
+)
